e74a13b34cca0cc81e691fc49dd2dda3d54d8149
paulpf <p.pfeifer@outlook.com>
2025-04-12 19:35:19 +0200

    refact: enhance loop functionality in IrrigationZone class and streamline main loop logic

--- src/irrigationZone.cpp
+++ src/irrigationZone.cpp
@@ -50,12 +50,6 @@ void IrrigationZone::synchronizeButtonStates(bool newState)
   _synchronizedBtnNewState = _swBtnState = _hwBtnState = newState;
 }
 
-void IrrigationZone::loop() 
-{
-  // Loop code for the irrigation zone
-  Trace::log("IrrigationZone loop running.");
-}
-
 void IrrigationZone::setupRelay(int relayGpioChannel) 
 {
   pinMode(relayGpioChannel, OUTPUT);
@@ -94,4 +88,40 @@ void IrrigationZone::startTimer()
 void IrrigationZone::resetTimer() 
 {
   _timerIsActive = false;
+}
+
+void IrrigationZone::loop() 
+{
+  // Loop code for the irrigation zone
+  Trace::log("IrrigationZone loop running.");
+
+  if (getRelayState() == false && getBtnState() == true)
+  {
+    setRelayState(true);
+
+    // Start timer
+    setDurationTime(10000); // 10 seconds
+    startTimer();
+  }
+  // if btn1 is inactive, relais 1 should be inactive
+  else if(getRelayState() == true && getBtnState() == false)
+  {
+    setRelayState(false);
+    resetTimer();
+  }
+
+  if (getRelayState())
+  {
+    int remainingTime = getRemainingTime();
+    Trace::log("Remaining time for relais1: " + String(remainingTime));
+    if (remainingTime <= 0)
+    {
+      Trace::log("Relais1 timer expired");
+      resetTimer();
+      setRelayState(false);
+      synchronizeButtonStates(false);
+    }
+  }
+
+  switchRelay(getRelayState());
 }
