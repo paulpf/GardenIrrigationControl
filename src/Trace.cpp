d11c6ad2753262616d7d72110e1e8cf094ea58b3
paulpf <p.pfeifer@outlook.com>
2025-04-12 13:33:11 +0200

    clear main branch for new beginning

--- src/Trace.cpp
+++
@@ -1,9 +0,0 @@
-#include "Trace.h"
-
-void Trace::log(String message)
-{
-  #ifdef TRACE
-  String timeStamp = String(millis());
-  Serial.println(timeStamp + " | " + message);
-  #endif
-}
--- src/_secrets/MqttSecret.h
+++
@@ -1,8 +0,0 @@
-#ifndef MQTT_SECRET_H
-#define MQTT_SECRET_H
-
-// MQTT Broker settings
-const char* MQTT_USER = "<mqtt_user>";
-const char* MQTT_PWD = "<mqtt_password>";
-
-#endif // MQTT_SECRET_H
